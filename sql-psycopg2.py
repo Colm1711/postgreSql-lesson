d986605565c2ef53be92be41d54f762bd4a41d4b
Colm1711 <cosullivan1711@gmail.com>
2022-07-23 14:39:45 +0000

    sqlalchemy expressions

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -22,7 +22,7 @@ cursor = connection.cursor()
 # Query 5 - select only by "ArtistId" from the "Album" table
 # cursor.execute('SELECT * FROM "Album" WHERE "ArtistId"=%s', [51])
 
-# Query 6 - select only by "ArtistId" from the "Album" table
+# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
 # cursor.execute('SELECT * FROM "Track" WHERE "Composer"=%s', ["Queen"])
 
 # fetch the results (multiple)
